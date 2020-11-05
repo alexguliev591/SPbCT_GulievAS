42fb84609513187e38d1fbbb87e84189268ac718
alexguliev591 <73433857+alexguliev591@users.noreply.github.com>
2020-11-05 22:14:52 +0300

    Laba_20
    Co-Authored-By: NIkolaevNikS <73941284+NIkolaevNikS@users.noreply.github.com>

---
+++ Laba20/Laba20/Laba20.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+#include "MathFuncsDll.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a,d;
+	double P = 3.14,s,p;
+	cout << "Введите радиус =	";
+	cin >> a;
+	p = 2 * MathFuncs::MyMathFuncs::Multiply(P, a);
+	s = P * MathFuncs::MyMathFuncs::Pow(a, 2);
+	cout << "Площадь = " << s << endl;
+	cout << "Периметр = " << p << endl;
+}
---
+++ Laba20/Laba20/MathFuncsDll.h
@@ -0,0 +1,25 @@
+// MathFuncsDll.h
+#ifdef MATHFUNCSDLL_EXPORTS
+#define MATHFUNCSDLL_API __declspec(dllexport)
+#else
+#define MATHFUNCSDLL_API __declspec(dllimport)
+#endif
+#pragma once
+
+namespace MathFuncs
+{
+	class MyMathFuncs
+	{
+	public:
+		// Returns a + b
+		static MATHFUNCSDLL_API double Add(double a, double b);
+		// Returns a - b
+		static MATHFUNCSDLL_API double Subtract(double a, double b);
+		// Returns a * b
+		static MATHFUNCSDLL_API double Multiply(double a, double b);
+		// Returns a / b
+		static MATHFUNCSDLL_API double Divide(double a, double b);
+
+		static MATHFUNCSDLL_API double Pow(double a, int b);
+	};
+}
